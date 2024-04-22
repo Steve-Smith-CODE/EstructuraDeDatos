5d78146f4dcf6dc6e3e0b792ac9daf2fa646464c
Steve-Smith-CODE <steven.ovallecito@gmail.com>
2024-04-22 15:52:54 -0500

    cambio2

--- EstructuradeDatos/Laboratorio2/Program.cs
+++ EstructuradeDatos/Laboratorio2/Program.cs
@@ -1,9 +1,8 @@
-﻿// sumar los n primeros
+﻿// Ejercicio-01: sumar los n primeros
 var n = 10;
 var suma = 0;
 for (var i = 1; i <= n; i++)
-{
+{   
     suma += i;
 }
 Console.WriteLine($"la suma de los {n} numeros son: {suma}");
-
---
+++ EstructuradeDatos/Laboratorio3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1. Declaración de variables
+        int num1;
+        int num2;
+        int suma;
+        int resta;
+        int multi;
+        int div;
+        //2. Pedir datos al usuario
+        Console.Write("Ingrese el primer número: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        //3. Realizar Operaciones
+        suma = num1 + num2;
+        resta = num1 - num2;
+        multi = num1 * num2;
+        div = num1 / num2;
+        //4. Mostrar resultado
+        Console.WriteLine("Adición: " + suma);
+        Console.WriteLine("Sustracción: " + resta);
+        Console.WriteLine("Multiplicación: " + multi);
+        Console.WriteLine("División: " + div);
+    }
+}
